d428433794b6095240d497347d87862ff3dce943
john lawless <jlawles4@vols.utk.edu>
2026-06-02 10:07:36 -0500

    Running on small

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -3,7 +3,8 @@ for i in "$1"/*; do
     filename=$(basename "$i")
     IFS='.' read -ra arrIN <<< "$filename"
 
-    python scripts/submit_slimmer.py -i filelists/$i -o $3/${arrIN[0]} --config config/config.json --wheel $2 --logdir ${i}_log
-    condor_submit ${i}_log/submit.sub
+    tag=${arrIN[0]}
+    python scripts/submit_slimmer.py -i $i -o $3/$tag --config config/config.json --wheel $2 --logdir ${tag}_log
+    condor_submit ${tag}_log/submit.sub
     sleep 2
 done
