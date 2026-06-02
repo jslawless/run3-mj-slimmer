7218f47bafde24b884d713a5c985ed6830846143
john lawless <jlawles4@vols.utk.edu>
2026-06-02 09:22:09 -0500

    Modifying the running to be a bit more sane

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+for i in "$1"/*; do
+    filename=$(basename "$i")
+    IFS='.' read -ra arrIN <<< "$filename"
 
-for i in $(ls $1); do
-	python scripts/submit_slimmer.py -i filelists/$i -o /store/group/lpcmultijets/johnny/slimmed_qcd_small  --config config/config.json --wheel $2 --logdir ${i}_log
-	condor_submit ${i}_log/submit.sub
-	sleep 2
+    python scripts/submit_slimmer.py -i filelists/$i -o $3/${arrIN[0]} --config config/config.json --wheel $2 --logdir ${i}_log
+    condor_submit ${i}_log/submit.sub
+    sleep 2
 done
